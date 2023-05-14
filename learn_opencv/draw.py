ea915d1a5e47bc478bea40478502e38ea87971a2
Shreyash <shreyashyaduvanshi@gmail.com>
2023-05-14 12:55:43 +0530

    draw

---
+++ learn_opencv/draw.py
@@ -0,0 +1,65 @@
+import cv2 as cv
+import numpy as np
+
+# # to create blank image for drawing
+# # dtype means data type and uint8 is data type of an image
+# (500, 500, 3) = (height, width, number of colour channels)
+blank = np.zeros((500, 500, 3), dtype='uint8')
+# cv.imshow('Blank', blank)
+
+# -------------------------------------------------
+# # image for drawing
+# img = cv.imread("learn_opencv\Photos\cat_smart.jfif")
+# cv.imshow('Cat', img)
+
+# ----------------------------------------------
+
+# # 1. Paint the image a certain colour
+# # blank[:] referencing all the pixels and setting equal to colour value
+# blank[:] = 0, 255, 0
+# blank[200:300, 300:400] = 0, 255, 0
+# cv.imshow('Green', blank)
+
+# -------------------------------------------------
+
+# # 2. Draw a rectangle
+
+# # this method takes in an image to draw the rectangle over
+
+# # rectangle(img, point1, point2, colour, thickness(2 is the thickness of the borders),...) 
+# # (0, 0) origin to (250, 250) with border of thickness 2 which is green in colour
+# cv.rectangle(blank, (0, 0), (250, 250), (0, 255, 0), thickness=2)
+# cv.rectangle(blank, (0, 0), (400, 250), (0, 255, 0), thickness=2)
+
+# # (cv.FILLED or -1) fills all the specified colour in the rectangle
+# cv.rectangle(blank, (0, 0), (400, 250), (0, 255, 0), thickness=cv.FILLED)
+# cv.rectangle(blank, (0, 0), (400, 250), (0, 255, 0), thickness=-1)
+
+# # (blank.shape[1] // 2) halfs the dimentions of the original image
+# cv.rectangle(blank, (0, 0), (blank.shape[1] // 2, blank.shape[0] // 2), (0, 250, 0), thickness=-1)
+
+# cv.imshow('Rectangle', blank)
+
+# -----------------------------------------------------------
+
+# # 3. Draw a circle
+
+# # circle(img, center, radius, colour, thickness, ...)
+# cv.circle(blank, (250, 250), 40, (0, 0, 255), thickness=3)
+# cv.circle(blank, (250, 250), 40, (0, 0, 255), thickness=-1)
+# cv.imshow('Circle', blank)
+
+# ------------------------------------------------------------
+
+# # 4. Draw a line
+# cv.line(blank, (100, 100), (250, 250), (255, 255, 255), thickness=3)
+# cv.imshow('Line', blank)
+
+# ------------------------------------------------------------
+
+# # 5. Write text
+# # cv.putText(img, text, origin, font face, font scale, colour, thickness, ...)
+# cv.putText(blank, 'Hello, I am Shreyash', (0, 225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0, 255, 0), thickness=2)
+# cv.imshow('Line', blank)
+
+# cv.waitKey(0)
---
+++ learn_opencv/read.py
@@ -0,0 +1,40 @@
+# Reading images and videos
+
+import cv2 as cv
+
+# # reading images
+# img = cv.imread("learn_opencv\Photos\cat_tongue.jfif")
+
+# # to display the image
+# # new window = cat, actual matrix of pixels to display = img
+# cv.imshow("Cat", img)
+
+# # keyboard binding function waits for a specific delay or a time in millisecons for a key to pressed
+# cv.waitKey(0)
+
+# ---------------------------------
+# this method either takes integer (0, 1, 3, etc.) for webcam or a path to a video file
+capture = cv.VideoCapture("learn_opencv\Videos\cat_meow.mp4")
+
+while True:
+
+    # capture.read() reads int this video frame by frame and returns the frame and a boolean to tell
+    # weather frame was succesfully read or not
+    isTrue, frame = capture.read()
+
+    # display an individual frame
+    cv.imshow("Video", frame)
+
+    # way to stop the video for playing indefinitely, if letter 'd' is pressed break out of this loop
+    # and stop playing this video
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+# to release capture pointer
+capture.release()
+cv.destroyAllWindows()
+
+# when video stops we get this error (-215:Assertion failed) which means open CV could not find a media
+# file at a particular location that you specified
+# reason: video ran out of frames as open CV could not find any more frames after the last frame in video.
+# So unexpectedly broke out of while loop by itself by raising a cv2 error.
